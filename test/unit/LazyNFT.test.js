f7bb72f588cc1648e6ee99db1608789e314975a5
jithendra-mallidu <jithendra.chowdary12@gmail.com>
2023-03-15 02:16:04 +0530

    initial commit

--- test/unit/LazyNFT.test.js
+++ test/unit/LazyNFT.test.js
@@ -1,73 +1,80 @@
-const { assert, expect } = require("chai")
-const { network, deployments, ethers } = require("hardhat")
-const { developmentChains } = require("../../helper-hardhat-config")
-const { createVoucher } = require("../../utils/mock-server")
+const { assert, expect } = require("chai");
+const { network, deployments, ethers } = require("hardhat");
+const { developmentChains } = require("../../helper-hardhat-config");
+const { createVoucher } = require("../../utils/mock-server");
 
 !developmentChains.includes(network.name)
-    ? describe.skip
-    : describe("LazyNFT Unit Tests", () => {
-          let LazyNft, deployer, player, voucher
+  ? describe.skip
+  : describe("LazyNFT Unit Tests", () => {
+      let LazyNft, deployer, player, voucher;
 
-          beforeEach(async () => {
-              const accounts = await ethers.getSigners()
-              deployer = accounts[0]
-              player = accounts[1]
+      beforeEach(async () => {
+        const accounts = await ethers.getSigners();
+        deployer = accounts[0];
+        player = accounts[1];
 
-              await deployments.fixture(["all"])
+        await deployments.fixture(["all"]);
 
-              LazyNft = await ethers.getContract("LazyNFT")
+        LazyNft = await ethers.getContract("LazyNFT");
 
-              voucher = await createVoucher(
-                  1,
-                  "www.google.com",
-                  ethers.utils.parseEther("1"),
-                  LazyNft
-              )
-          })
+        voucher = await createVoucher(
+          1,
+          "www.google.com",
+          ethers.utils.parseEther("1"),
+          LazyNft
+        );
+      });
 
-          describe("LazyNFT Constructor is set correctly", () => {
-              it("sets the name to Test Lazy NFT Studio", async () => {
-                  const name = await LazyNft.name()
-                  assert.equal(name, "Test Lazy NFT Studio")
-              })
+      describe("LazyNFT Constructor is set correctly", () => {
+        it("sets the name to Test Lazy NFT Studio", async () => {
+          const name = await LazyNft.name();
+          assert.equal(name, "Test Lazy NFT Studio");
+        });
 
-              it("sets the symbol to TLFS", async () => {
-                  const symbol = await LazyNft.symbol()
-                  assert.equal(symbol, "TLFS")
-              })
+        it("sets the symbol to TLFS", async () => {
+          const symbol = await LazyNft.symbol();
+          assert.equal(symbol, "TLFS");
+        });
 
-              it("sets the creatorRole to deployer", async () => {
-                  const creatorRole = ethers.utils.keccak256(
-                      ethers.utils.toUtf8Bytes("CREATOR_ROLE")
-                  )
+        it("sets the creatorRole to deployer", async () => {
+          const creatorRole = ethers.utils.keccak256(
+            ethers.utils.toUtf8Bytes("CREATOR_ROLE")
+          );
 
-                  expect(await LazyNft.hasRole(creatorRole, deployer.address)).to.be.true
-              })
-          })
+          expect(await LazyNft.hasRole(creatorRole, deployer.address)).to.be
+            .true;
+        });
+      });
 
-          describe("LazyNFT redeem", () => {
-              it("mints an NFT for a valid voucher", async () => {
-                  const LazyNftPlayer = LazyNft.connect(player)
-                  const redeemTransaction = await LazyNftPlayer.redeem(player.address, voucher, {
-                      value: ethers.utils.parseEther("1"),
-                  })
-                  await redeemTransaction.wait(1)
+      describe("LazyNFT redeem", () => {
+        it("mints an NFT for a valid voucher", async () => {
+          const LazyNftPlayer = LazyNft.connect(player);
+          const redeemTransaction = await LazyNftPlayer.redeem(
+            player.address,
+            voucher,
+            {
+              value: ethers.utils.parseEther("1"),
+            }
+          );
+          await redeemTransaction.wait(1);
 
-                  const playerLazyNftBalance = await LazyNftPlayer.balanceOf(player.address)
+          const playerLazyNftBalance = await LazyNftPlayer.balanceOf(
+            player.address
+          );
 
-                  assert(playerLazyNftBalance.toString(), "1")
-              })
+          assert(playerLazyNftBalance.toString(), "1");
+        });
 
-              it("reverts if the voucher is tampered with", async () => {
-                  const tamperedVoucher = {
-                      ...voucher,
-                      tokenId: 2,
-                      minPrice: 0,
-                      uri: "www.youtube.com",
-                  }
-                  await expect(LazyNft.redeem(player.address, tamperedVoucher)).to.be.revertedWith(
-                      "LazyNFT__UnauthorizedCreator"
-                  )
-              })
-          })
-      })
+        it("reverts if the voucher is tampered with", async () => {
+          const tamperedVoucher = {
+            ...voucher,
+            tokenId: 2,
+            minPrice: 0,
+            uri: "www.youtube.com",
+          };
+          await expect(
+            LazyNft.redeem(player.address, tamperedVoucher)
+          ).to.be.revertedWith("LazyNFT__UnauthorizedCreator");
+        });
+      });
+    });
